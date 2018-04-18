9437b2cba19887de932ecfb90a8c9653059995d9
Fred Mayer <fedor-majer@yandex.ru>
2018-04-18 21:56:21 +0300

    Add HTTP response url validator

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,17 +6,28 @@ use App\Url;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Validator;
 
 class ApiController extends Controller
 {
     //
 
     public function postUrl(Request $request){
-        $this->validate($request, [
+
+        $validator = Validator::make($request->all(), [
             'url' => 'required|url|max:255',
         ]);
-
         $url = $request->input('url');
+
+        $validator->after(function($validator) use ($url) {
+            if (!Url::checkOriginUrl($url)) {
+                $validator->errors()->add('url', 'Can not access url');
+            }
+        });
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
 
         //TODO:: model url maker
         $model = Url::generate($url);
--- app/Url.php
+++ app/Url.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\Helpers\StringHelper;
 use Illuminate\Database\Eloquent\Model;
+use Ixudra\Curl\Facades\Curl;
 
 class Url extends Model
 {
@@ -14,6 +15,11 @@ class Url extends Model
     public $timestamps = false;
     protected $fillable = ['url', 'short', 'expired'];
 
+    /**
+     * Генерация и запись короткого Url
+     * @param $url string
+     * @return object
+     */
     public static function generate($url){
         $short = StringHelper::getInstance()->createRandomString();
 
@@ -30,6 +36,17 @@ class Url extends Model
             'short' => $short,
             'expired' => $expired->format('Y-m-d H:i:s'),
         ]);
+    }
+
+    public static function checkOriginUrl($url){
+        $res = Curl::to($url)
+            ->returnResponseObject()
+            ->get();
+
+        if($res->status == 200){
+            return true;
+        }
+        return false;
     }
 
     public function getShort(){
